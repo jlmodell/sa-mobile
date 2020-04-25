6cd506d809e1acd36a5e9ef065f7f5e5a0a7fac0
jeff <modell.jeff@me.com>
2020-04-25 16:25:51 -0400

    update changes suggested by jpg

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -19,13 +19,16 @@ const mapSizesToProps = ({ width }) => ({
 
 var placeholder;
 var title;
+var limiter;
 
 if (process.env.REACT_APP_DUMMY) {
     title = "Sample Data Fetch and Display"
     placeholder = "Enter an Item ID:"          //TODO, search by ID or Name
+    limiter = 0
 } else {
     placeholder = "Enter an Item ID: (ROI ID)"
     title = "Busse Hospital Disposables"
+    limiter = 0
 }
 
 const Dashboard = observer(({isMobile}) => {
@@ -33,6 +36,7 @@ const Dashboard = observer(({isMobile}) => {
     const [valid, isValid] = React.useState(false)
     const dates = `${dateHelper(query.start)} - ${dateHelper(query.end)}`
     const [item, setItem] = React.useState('')
+    const [description, setDescription] = React.useState('')
     const [items, setItems] = React.useState([])
     const [active, setActive] = React.useState('')
     const itemRef = React.useRef(null);
@@ -42,36 +46,51 @@ const Dashboard = observer(({isMobile}) => {
 		auth.isAuth && itemRef.current.focus();
     }, []);
 
-    const fetch = async (item) => {
-        const res = await query.fetch_item_description(item)
-        setItems(res)
+    const validItem = async (item) => {
+        if (item.length > limiter) {
+            const res = await query.fetch_item_description(item)
+
+            setItems(res)
+
+            const regex = new RegExp(`^${item}$`)
+            const match = res.find(i => regex.test(i.item_id))
+
+            if (match) {
+                setActive(match.item_id)
+                setDescription(match.item_description)
+                isValid(true)
+                setItems(items.filter(i => match.item_id === i.item_id))
+            } else {
+                setActive('')
+                setDescription('')
+                isValid(false)
+            }
+        }
     }
 
-    const validItem = (items, item) => {
-        return items.some(i => i.item_id === item)
-    }
-    
     React.useEffect(
-        () => {            
-            if (process.env.REACT_APP_DUMMY) {
-                if (item.length >= 1) {
-                    fetch(item)
-                }
-            } else {
-                if (item.length > 2) {
-                    fetch(item)
-                }           
-            }     
+        () => {
+            console.log(item)
+            validItem(item)
         },
         [item]
-    );    
+    )
+
+    React.useEffect(
+        () => {
+            if (active === '') {
+                query.dataIsLoaded = false
+            }
+        },
+        [active]
+    )
 
     return (
-        <div className="dashboard disable-scrollbars">        
+        <div className="dashboard disable-scrollbars">
             <section id="header">
                 <nav className="nav grid">
                     <h1>{title}</h1>
-                    
+
                     <ul>
                         <li><a href="#options-selector">Data Selector</a></li>
                         <li><a href="#kpi">KPI</a></li>
@@ -79,24 +98,25 @@ const Dashboard = observer(({isMobile}) => {
                         <li><a href="#table">Tables</a></li>
                     </ul>
                 </nav>
-                <div className="github-logo"><a href="https://github.com/jlmodell/sa-mobile" _taget="blank"><img src={github} alt="github" height="64"/></a></div>
+                <div className="github-logo"><a href="https://github.com/jlmodell/sa-mobile" _target="blank"><img src={github} alt="github" height="64"/></a></div>
             </section>
             <section id="options-selector">
-                <div className="options-outer-container">                    
-                    <div className="options-container">                        
+                <div className="options-outer-container">
+                    <div className="options-container">
                         <div className="options grid">
-                            <button 
-                                    disabled={valid && query.start !== '' && query.end !== '' ? false : true} 
-                                    className={valid && query.start !== '' && query.end !== '' ? "options" : "options disabled"} 
-                                    onClick={() => {                                                         
-                                        query.fetch_data()                                        
+                            <button
+                                    disabled={valid && query.start !== '' && query.end !== '' ? false : true}
+                                    className={valid && query.start !== '' && query.end !== '' ? "options" : "options disabled"}
+                                    onClick={() => {
+                                        query.fetch_data()
                                     }}
                                 >
-                                Refresh Data
+                                Fetch Data
                             </button>
-                            <input className={valid ? "options" : "options invalid"} autocomplete="off" placeholder={placeholder} ref={itemRef} name="item" type="text" value={item} onChange={(e) => {
+                            <input className={valid ? "options" : "options invalid"} autoComplete="off" placeholder={placeholder} ref={itemRef} name="item" type="text" value={item} onChange={(e) => {
                                 setItem(e.target.value)
-                                localStorage.setItem('item', e.target.value);                                
+                                query.item = e.target.value
+                                localStorage.setItem('item', e.target.value)
                             }} />
                             <label className="options options-label" htmlFor="start">Start</label>
                             <input className="options" ref={startRef} name="start" type="date" value={query.start} onChange={(e) => {
@@ -107,31 +127,30 @@ const Dashboard = observer(({isMobile}) => {
                             <input className="options" name="end" type="date" value={query.end} onChange={(e) => {
                                 query.end = e.target.value
                                 localStorage.setItem('end', e.target.value);
-                            }} />                                                                               
+                            }} />
                         </div>
 
-                        {items && item.length >=1 && <div className="options-container-list">
-                            <div style={{color:"white"}} className="chart-title">Choose (1) One.</div>
-                            <ul>                                
+                        {!query.dataIsLoaded && items && item.length >=1 && <div className="options-container-list">
+                            <div style={{color:"white"}} className="choose-one">Choose (1) One.</div>
+                            <ul>
                                 {items.map((item,index) => (
-                                    <li 
-                                        key={index} 
+                                    <li
+                                        key={index}
                                         className={active === item.item_id ? "options-container-li-active" : "options-container-li"}
                                         onClick={() => {
                                             setActive(item.item_id)
                                             setItem(item.item_id)
-                                            if (validItem(items, item.item_id)) isValid(true)
-                                            setItems(items.filter(i => item.item_id === i.item_id))                                                                           
+                                            setItems(items.filter(i => item.item_id === i.item_id))
                                             query.item = item.item_id
                                         }}
                                     >
                                         <span className="options-container-li-iid">[{item.item_id}]</span> {item.item_description}
                                     </li>
                                 ))}
-                            </ul>     
+                            </ul>
                         </div>}
 
-                    </div>                    
+                    </div>
 
                     <div className={query.dataIsLoaded && !isMobile ? "scrolldown" : ""}>
                         <span></span>
@@ -139,20 +158,21 @@ const Dashboard = observer(({isMobile}) => {
                         <span></span>
                     </div>
                 </div>
-                
+
             </section>
 
             {query.noSales &&
                 <div className="sub-title">
-                    <h2>No sales were found during {dates}.</h2>
+                    <h2>No sales of {active} - {description} were found during {dates}.</h2>
                 </div>
             }
 
-            {(!query.isLoading && query.dataIsLoaded) && !query.noSales &&
+            {(!query.isLoading && query.dataIsLoaded && active !== "") && !query.noSales &&
                 (
                     <>
                         <section id="kpi">
                             <div className="kpi-outer-container">
+                                <div className="title"><h2>[{active}] {description}</h2></div>
                                 <div className="sub-title"><p>{dates}</p></div>
                                 <div className="kpi-container grid">
                                     <div className="kpi-container kpi-boxes">
@@ -237,10 +257,10 @@ const Dashboard = observer(({isMobile}) => {
                                                     maximumFractionDigits: 0
                                                 }
                                             )}
-                                        </p>    
+                                        </p>
                                         <p>
                                             {query.item_data.sales !== 0 ? (((query.item_data.sales - query.item_data.costs - query.item_data.tradefees - query.item_data.rebates - query.item_data.freight - query.item_data.overhead)/query.item_data.sales)*100)
-                                                .toFixed(2) : "N/A"    
+                                                .toFixed(2) : "N/A"
                                             }% of Sales
                                         </p>
                                     </div>
@@ -269,38 +289,38 @@ const Dashboard = observer(({isMobile}) => {
                                                 }
                                             ) : "N/A"}
                                         </p>
-                                    </div>                                    
-                                </div>                                    
+                                    </div>
+                                </div>
                             </div>
-                        </section>                        
+                        </section>
                         <section id="charts">
                             <div className="chart-container grid">
                                 <div className="chart-container">
-                                    <div className="chart-title"><h2>Breakdown of Sale</h2></div>
+                                    <div className="chart-title"><h2>Breakdown of Cost Centers of [{active}] {description}</h2></div>
                                     <div className="sub-title"><p>{dates}</p></div>
                                     <div className="chart">
                                         <SimpleRadarChart/>
                                     </div>
                                 </div>
                                 <div className="chart-container">
-                                    <div className="chart-title"><h2>Sales Over Time</h2></div>
+                                    <div className="chart-title"><h2>Sales Over Time of [{active}] {description}</h2></div>
                                     <div className="sub-title"><p>{dates}</p></div>
                                     <div className="chart">
                                         <SimpleBarChart />
                                     </div>
-                                </div>                                                
+                                </div>
                                 <div className="chart-container">
-                                    <div className="chart-title"><h2>Breakdown of Customers</h2></div>    
-                                    <div className="sub-title"><p>{dates}</p></div>                                
-                                    <div className="chart">                                    
+                                    <div className="chart-title"><h2>Breakdown of Customers</h2></div>
+                                    <div className="sub-title"><p>{dates}</p></div>
+                                    <div className="chart">
                                         <SimplePieChart />
                                     </div>
                                 </div>
                             </div>
-                        </section>                        
+                        </section>
                         <section id="table">
                             <div className="data-table-container grid">
-                                <div className="data-table-title"><h2>Sales Details</h2></div>
+                                <div className="data-table-title"><h2>Sales Details of [{active}] {description}</h2></div>
                                 <div className="sub-title"><p>{dates}</p></div>
                                 <div className="data-table">
                                     <SimpleDataTable />
